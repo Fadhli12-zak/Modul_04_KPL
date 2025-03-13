d8fca28467252912943bd1164ed2f7a376ef9caf
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-13 10:55:22 +0700

    menambahkan class FanLaptop

--- Program.cs
+++ Program.cs
@@ -30,11 +30,77 @@ class MainProgram {
             {
                 Console.WriteLine("Kode produk: " + input + " adalah " + Search);
             }
-            else { 
+            else
+            {
                 Console.WriteLine("Produk tidak ditemukan");
             }
         }
-
+        FanLaptop fanLaptop = new FanLaptop();
+        fanLaptop.FanMod();
     } 
-    
 }
+class FanLaptop
+{
+    enum State { QUIET, BALANCED, PERFORMANCE, TURBO };
+    public void FanMod()
+    {
+        State state = State.QUIET;
+        string command = null;
+        while (command != "exit") { 
+            Console.WriteLine("Masukkan perintah: ");
+            command = Console.ReadLine();
+            switch (state)
+            {
+                case State.QUIET:
+                    if (command == "Mode Up")
+                    {
+                        state = State.BALANCED;
+                        Console.WriteLine("Fan Quiet berubah menjadi Balanced");
+                    }
+                    else if (command == "Turbo Shortcut")
+                    {
+                        state = State.TURBO;
+                        Console.WriteLine("Fan Quiet Berubah menjadi turbo");
+                    }
+                    break;
+                case State.BALANCED:
+                    if (command == "Mode Up")
+                    {
+                        state = State.PERFORMANCE;
+                        Console.WriteLine("Fan Balance berubah menjadi Performance");
+                    }
+                    else if (command == "Mode Down")
+                    {
+                        state = State.QUIET;
+                        Console.WriteLine("Fan Balance berubah menjadi quiet");
+                    }
+                    break;
+                case State.PERFORMANCE:
+                    if (command == "Mode Up")
+                    {
+                        state = State.TURBO;
+                        Console.WriteLine("Fan performance berubah menjadi turbo");
+                    }
+                    else if (command == "Mode Down")
+                    {
+                        state = State.BALANCED;
+                        Console.WriteLine("Fan performance berubah menjadi balance");
+                    }
+                    break;
+                case State.TURBO:
+                    if (command == "Mode Down")
+                    {
+                        state = State.PERFORMANCE;
+                        Console.WriteLine("Fan turbo Berubah menjadi performance");
+                    }else if (command == "Turbo Shortcut")
+                    {
+                        state = State.QUIET;
+                        Console.WriteLine("Fan turbo Berubah menjadi Quiet");
+                    }
+                    break;
+            }
+        }
+
+    }
+}
+ 
